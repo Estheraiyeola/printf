44159f2bcd571ad1bf8892eea92bf516a7ad9a7c
Estheraiyeola <estheraiyeola@yahoo.com>
2023-03-28 10:44:00 -0700

    Added printf function

--- _printf.c
+++ _printf.c
@@ -1,8 +1,9 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _printf - produces output to a format
- * @format: A character string
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
  * Return: A total count of the characters printed
  */
 int _printf(const char *format, ...)
@@ -26,54 +27,11 @@ int _printf(const char *format, ...)
 	va_list arg_list;
 
 	if (format == NULL)
-	{
-<<<<<<< HEAD
 		return (-1);
-=======
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind)
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
->>>>>>> ebf5997cba0fc418576da0489dc8e651bd27c089
-	}
-	va_start(arg_list, format);
 
+	va_start(arg_list, format);
+	/*Calling parser function*/
 	printed_chars = parser(format, f_list, arg_list);
 	va_end(arg_list);
 	return (printed_chars);
 }
-<<<<<<< HEAD
-=======
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
-
->>>>>>> ebf5997cba0fc418576da0489dc8e651bd27c089
